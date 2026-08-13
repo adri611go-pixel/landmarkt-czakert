4e56a1c65a3dd87b18a57917b84ef4d09c2adcf0
adri611go-pixel <adri.611.go@gmail.com>
2026-08-13 14:05:05 +0100

    löschen der Dopplung

--- FINALE_VERSION_WEBSEITE_GITHUB_2026-08-13_NEU/work/site-preview/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: "export",
-  images: { unoptimized: true },
-  allowedDevOrigins: ["127.0.0.1", "localhost"],
-};
-
-export default nextConfig;
